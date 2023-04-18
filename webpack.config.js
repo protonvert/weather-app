302ae2dc7e7fc8ddf423c08a36cb53396e78d360
Tristen <79072811+protonvert@users.noreply.github.com>
2023-04-17 20:42:27 -0500

    initial commit

---
+++ webpack.config.js
@@ -0,0 +1,28 @@
+const path = require('path')
+
+const config = {
+  entry: './src/index.js',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'bundle.js'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        use: 'babel-loader',
+        exclude: /node_modules/
+      },
+      {
+        test: /\.less$/,
+        use: [
+          'style-loader',
+          'css-loader',
+          'less-loader'
+        ]
+      }
+    ]
+  }
+}
+
+module.exports = config
